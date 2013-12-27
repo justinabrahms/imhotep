07c9aab61d45062ba5594270939a03fd9cc172c7
Justin Abrahms <justin@abrah.ms>
2013-12-26 18:01:55 -0800

    better fleshing out higher level interface.
    Need to think more about pylint.

--- imhotep/tools.py
+++ imhotep/tools.py
@@ -9,19 +9,34 @@ class Tool(object):
     def __init__(self, command_executor):
         self.executor = command_executor
 
-    def process_line(self, line):
-        return []
+    def process_line(self, dirname, line):
+        """
+        Processes a line return a 3-element tuple representing (filename,
+        line_number, error_messages) or None to indicate no error.
 
-    def get_file_extension(self):
+        :param: dirname - directory the code is running in
+        """
+        raise NotImplementedError()
+
+    def get_file_extensions(self):
         """
         Returns a list of file extensions this tool should run against.
 
         eg: ['.py', '.js']
         """
-        return ['']
+        raise NotImplementedError()
+
+    def get_command(self, dirname):
+        """
+        Returns the command to run for linting. It is piped a list of files to
+        run on over stdin.
+        """
+        raise NotImplementedError()
 
     def invoke(self, dirname, filenames=set()):
         """
+        Main entrypoint for all plugins.
+        
         Returns results in the format of:
 
         {'filename': {
@@ -33,29 +48,39 @@ class Tool(object):
         }
 
         """
-        raise NotImplementedError
+        retval = defaultdict(lambda: defaultdict(list))
+        extensions = ' -o '.join(['-name "*.%s"' % ext for ext in
+                                  self.get_file_extensions()])
 
+        cmd = 'find %s %s | xargs %s' % (
+            dirname, extensions, self.get_command())
+        result = self.executor(cmd)
+        for line in result.split('\n'):
+            output = process_line(dirname, line)
+            if output is not None:
+                filename, lineno, messages = output
+                retval[filename][lineno].append(messages)
+        return retval
 
 class JSHint(Tool):
     response_format = re.compile(r'^(?P<filename>.*): line (?P<line_number>\d+), col \d+, (?P<message>.*)$')
     jshintrc_filename = '.jshintrc'
 
-    def invoke(self, dirname, filenames=set()):
-        to_return = defaultdict(lambda: defaultdict(list))
-        cmd = 'find %s -name "*.js" | ' \
-          " xargs jshint " % dirname
-        jshint_file = os.path.join(dirname, self.jshintrc_filename)
-        if os.path.exists(jshint_file):
-            cmd += "--config=%s" % jshint_file
-        result = self.executor(cmd)
-        # format:
-        # cssauron/index.js: line 87, col 12, Missing semicolon.
-        for l in result.split("\n"):
-            line = l[len(dirname)+1:] # +1 for trailing slash to make relative dir
-            match = self.response_format.search(line)
-            if match is not None:
-                to_return[match.group('filename')][match.group('line_number')].append(match.group('message'))
-        return to_return
+    def process_line(self, dirname, line):
+        line = line[len(dirname)+1:] # +1 for trailing slash to make relative dir
+        match = self.response_format.search(line)
+        if match is not None:
+            return match.groups()
+
+    def get_file_extensions(self):
+        return ['.js']
+
+    def get_command(self, dirname):
+        cmd = "jshint "
+        config_path = os.path.join(dirname, jshint_file)
+        if os.path.exists(config_path):
+            cmd += "--config=%s" % config_path
+        return cmd
 
 
 class PyLint(Tool):
