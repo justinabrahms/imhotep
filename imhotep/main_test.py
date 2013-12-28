17ca26c70ac27bdf59c3bb2e4aafa42e6f62007b
Justin Abrahms <justin@abrah.ms>
2013-12-28 12:09:23 -0800

    test for repomanager fetching correct repo class.

--- imhotep/main_test.py
+++ imhotep/main_test.py
@@ -1,6 +1,17 @@
-from main import load_config
+from main import load_config, RepoManager
+from repositories import Repository, AuthenticatedRepository
 
 
 def test_config_loading():
     c = load_config('doesnt_exist')
     assert isinstance(c, dict)
+
+
+def test_authencticated_repo():
+    r = RepoManager(authenticated=True)
+    assert AuthenticatedRepository == r.get_repo_class()
+
+
+def test_unauthencticated_repo():
+    r = RepoManager()
+    assert Repository == r.get_repo_class()
