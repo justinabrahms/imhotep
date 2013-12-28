b4da0629c00c174bd5feab3f4d947f040bc63200
Justin Abrahms <justin@abrah.ms>
2013-12-28 12:02:03 -0800

    Now with passing tests.

--- imhotep/repositories_test.py
+++ imhotep/repositories_test.py
@@ -2,14 +2,18 @@ from imhotep.repositories import Repository, AuthenticatedRepository
 
 repo_name = 'justinabrahms/imhotep'
 
+
 def test_unauthed_download_location():
-    uar = Repository(repo_name, None, None)
-    assert uar.download_location == "git://github.com/justinabrahms/imhotep.git"
+    uar = Repository(repo_name, None, None, None)
+    loc = uar.download_location
+    assert loc == "git://github.com/justinabrahms/imhotep.git"
+
 
 def test_authed_download_location():
-    ar = AuthenticatedRepository(repo_name, None, None)
+    ar = AuthenticatedRepository(repo_name, None, None, None)
     assert ar.download_location == "git@github.com:justinabrahms/imhotep.git"
 
+
 def test_unicode():
-    r = Repository(repo_name, None, None)
+    r = Repository(repo_name, None, None, None)
     assert r.__unicode__() == repo_name
