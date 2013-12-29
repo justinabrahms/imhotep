75c3f5e588f4698c034367b6addcbd9212c1a450
Justin Abrahms <justin@abrah.ms>
2013-12-28 19:29:17 -0800

    Add debug logging to http utils

--- imhotep/http.py
+++ imhotep/http.py
@@ -1,6 +1,9 @@
 import requests
 import json
 from requests.auth import HTTPBasicAuth
+import logging
+
+log = logging.getLogger(__name__)
 
 
 class GithubRequester(object):
@@ -15,9 +18,11 @@ class GithubRequester(object):
         return HTTPBasicAuth(self.username, self.password)
 
     def get(self, url):
+        log.debug("Fetching %s", url)
         return requests.get(url, auth=self.get_auth())
 
     def post(self, url, payload):
+        log.debug("Posting %s to %s", payload, url)
         return requests.post(
             url, data=json.dumps(payload),
             auth=self.get_auth())
