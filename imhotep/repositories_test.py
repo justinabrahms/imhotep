dc68f1f65393fc30443f96b2e327e18fd50fd5e9
Justin Abrahms <justin@abrah.ms>
2014-01-18 13:50:51 -0800

    Increase repo tests

--- imhotep/repositories_test.py
+++ imhotep/repositories_test.py
@@ -1,3 +1,4 @@
+import mock
 from imhotep.repositories import Repository, AuthenticatedRepository
 
 repo_name = 'justinabrahms/imhotep'
@@ -17,3 +18,23 @@ def test_authed_download_location():
 def test_unicode():
     r = Repository(repo_name, None, [None], None)
     assert r.__unicode__() == repo_name
+
+
+def test_diff_commit():
+    executor = mock.Mock()
+    uar = Repository(repo_name, '/loc/', [None], executor)
+    uar.diff_commit('commit-to-diff')
+    executor.assert_called_with("cd /loc/ && git diff commit-to-diff")
+
+
+def test_diff_commit__compare_point_applied():
+    executor = mock.Mock()
+    uar = Repository(repo_name, '/loc/', [None], executor)
+    uar.diff_commit('commit-to-diff', compare_point='base')
+    executor.assert_any_call("cd /loc/ && git checkout base")
+
+def test_apply_commit():
+    executor = mock.Mock()
+    uar = Repository(repo_name, '/loc/', [None], executor)
+    uar.apply_commit('base')
+    executor.assert_called_with("cd /loc/ && git checkout base")
