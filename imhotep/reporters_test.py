47ed51df8b0ef5c9c2566a311a5fc95d11cc4381
Justin Abrahms <justin@abrah.ms>
2014-02-22 12:35:34 -0800

    Update tests to fit the new API

--- imhotep/reporters_test.py
+++ imhotep/reporters_test.py
@@ -32,10 +32,8 @@ def test_get_comments_no_cache():
     requester = mock.MagicMock()
     requester.get.return_value.json = return_data
     requester.get.return_value.status_code = 200
-    pr = GitHubReporter()
-    pr.requester = requester
-    pr.report_url = 'example.com'
-    result = pr.get_comments()
+    pr = GitHubReporter(requester)
+    result = pr.get_comments('example.com')
     assert result == return_data
     assert pr._comments == return_data
     requester.get.assert_called_with('example.com')
@@ -44,9 +42,9 @@ def test_get_comments_no_cache():
 def test_get_comments_cache():
     return_data = {'foo': 'bar'}
     requester = mock.MagicMock()
-    pr = GitHubReporter()
+    pr = GitHubReporter(requester)
     pr._comments = return_data
-    result = pr.get_comments()
+    result = pr.get_comments('example.com')
     assert result == return_data
     assert not requester.get.called
 
@@ -54,18 +52,15 @@ def test_get_comments_cache():
 def test_get_comments_error():
     requester = mock.MagicMock()
     requester.get.return_value.status_code = 400
-    pr = GitHubReporter()
-    pr.requester = requester
-    pr.report_url = 'example.com'
-    result = pr.get_comments()
+    pr = GitHubReporter(requester)
+    result = pr.get_comments('example.com')
     assert result is None
 
 
 def test_clean_already_reported():
     requester = mock.MagicMock()
     requester.username = 'magicmock'
-    pr = GitHubReporter()
-    pr.requester = requester
+    pr = GitHubReporter(requester)
     comments = [{'path': 'foo.py',
                  'position': 2,
                  'body': 'Get that out',
