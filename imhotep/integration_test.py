1811f9198eab66ab55255d00cd56cc8415dae57e
Justin Abrahms <justin@abrah.ms>
2014-02-22 11:59:00 -0800

    formatting

--- imhotep/integration_test.py
+++ imhotep/integration_test.py
@@ -13,9 +13,12 @@ delete comment: DELETE /repos/:owner/:repo/pulls/comments/:number
                 http://developer.github.com/v3/pulls/comments/#delete-a-comment
 """
 import os
+
 import pytest
+
 from imhotep.http import GithubRequester
 from imhotep.reporters import PRReporter
+
 
 ghu = os.getenv('GITHUB_USERNAME')
 ghp = os.getenv('GITHUB_PASSWORD')
@@ -33,12 +36,7 @@ def test_github_post():
     test_str = 'integration test error name'
     req = GithubRequester(ghu, ghp)
     r = PRReporter(req, pr)
-    r.report_line(repo,
-                  'da6a127',
-                  'foo.py',
-                  2,
-                  3, # the first 'pass' line.
-                  test_str)
+    r.report_line(repo, 'da6a127', 'foo.py', 2, 3, test_str)
     comments = req.get('https://api.github.com/repos/%s/pulls/%s/comments' %
                        (repo, pr)).json
     posted = [x for x in comments if test_str in x['body']]
