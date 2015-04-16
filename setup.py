5e812cb43b459b74e779274ceca4141295be56b5
Justin Abrahms <justin@abrah.ms>
2015-04-16 09:41:00 -0700

    install six on pip installation.

--- setup.py
+++ setup.py
@@ -2,14 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='imhotep',
-    version='0.2.0',
+    version='0.2.1',
     packages=find_packages(),
     url='https://github.com/justinabrahms/imhotep',
     license='MIT',
     author='Justin Abrahms',
     author_email='justin@abrah.ms',
     description='A tool to pipe linters into code review',
-    install_requires=['requests==2.6.0'],
+    install_requires=[
+        'requests==2.6.0',
+        'six',
+    ],
     extras_require={'pylint': 'PyLint'},
     entry_points = {
         'console_scripts': [
