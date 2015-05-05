193952a62eb99e549ad6e2912c46d37cf8f8faf7
Justin Abrahms <justin@abrah.ms>
2015-05-04 23:02:00 -0700

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='imhotep',
-    version='0.3.0',
+    version='0.3.1',
     packages=find_packages(),
     url='https://github.com/justinabrahms/imhotep',
     license='MIT',
