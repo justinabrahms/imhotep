b4e42f0c1da8417d3e03533c2398a99806d2e651
Justin Abrahms <justin@abrah.ms>
2015-05-04 22:49:47 -0700

    version bump for better clone urls

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='imhotep',
-    version='0.2.1',
+    version='0.3.0',
     packages=find_packages(),
     url='https://github.com/justinabrahms/imhotep',
     license='MIT',
