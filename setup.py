dfe618d28f80181a4c7a039086562c61021e6d2f
Justin Abrahms <justin@abrah.ms>
2016-03-24 19:13:27 -0400

    v1.0.1
    - Fixes API use due to lack of specs on mocks in testing.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='imhotep',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     url='https://github.com/justinabrahms/imhotep',
     license='MIT',
